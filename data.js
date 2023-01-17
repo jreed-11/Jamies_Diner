e3575d09e8e1203b1f76d754e75038deae9f9cbb
Jamie Reed <98095541+jreed-11@users.noreply.github.com>
2023-01-17 20:57:19 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        image: "images/pizza_image.png"
+    },
+    {
+        name: "Cheeseburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "images/burger_image.png",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        image: "images/beer_image.png",
+        id: 2
+    }
+]
---
+++ index.js
@@ -0,0 +1,118 @@
+import {menuArray} from '/data.js'
+
+const totalPriceEl = document.getElementById('total-price')
+
+let myOrderArray = [];
+let totalPrice = 0;
+
+
+
+
+document.addEventListener('click', function(e){
+  if(e.target.dataset.item) {
+    handleMenuClick(e.target.dataset.item)
+  }
+  
+})
+
+// Click Functions //
+
+function handleMenuClick(itemId) {
+  let targetItem = menuArray.filter(function(item){
+    return  parseInt(itemId) === item.id
+  })[0]
+
+  
+
+    if(!myOrderArray.includes(targetItem)){
+      myOrderArray.push(targetItem)
+      targetItem.quantity =1
+    } else {
+      targetItem.quantity++
+    }
+
+    totalPrice += targetItem.price
+    totalPriceEl.textContent =`£${totalPrice}`
+
+    // if(myOrderArray.length > 0) {
+    //   // document.getElementById('order-section').classList.remove('hidden')
+    //   // document.getElementById('order-display').classList.remove('hidden')
+      
+    // }
+
+    // document.getElementById('your-order').classList.remove('hidden')
+    // document.getElementById('order-item-details').classList.remove('hidden')
+    // document.getElementById('item-quantity').textContent = `x${targetItem.quantity}`
+    render()
+
+}
+
+// Get Menu Items //
+
+function getMenuItems(){
+
+let menuItems = ``
+
+menuArray.forEach(function(item){
+    
+    menuItems += `
+    <div class="menu-item">
+        <div class="item-wrapper">
+            <img src="${item.image}" class="item-img">
+                <div class="item-detail" id="item-detail">  
+                    <p class="item-name">${item.name}</p>
+                    <p class="item-ingredients">${item.ingredients}</p>
+                    <p class="item-price">£${item.price}</p>
+                </div>
+        </div>    
+            <button class="item-btn" id="item-btn" data-item="${item.id}">+</button> 
+        </div>
+  
+    <div class="item-divider"></div>
+    `  
+})
+    return menuItems
+    
+}    
+
+// My Order Section //
+
+function yourOrder(){
+
+  let orderHtml = ''
+
+  myOrderArray.forEach(function(item){
+
+    orderHtml += 
+    `
+      <div class="your-order" id="your-order">
+          
+          <div class="order-item-details">     
+            <p class="order-item-name">${item.name}</p>
+            <button class="order-remove-btn" data-remove"${item.id}">Remove</button>
+            <p class="item-quantity">x ${item.quantity}</p>        
+          </div>
+
+          <div class="order-price-details">
+            <p class="item-price">£${item.price * item.quantity}</p>
+          </div>
+        
+      </div>
+
+    
+    `
+  })
+
+  return orderHtml
+
+}
+
+//Render to HTML //
+
+function render(){
+        document.getElementById('menu-section').innerHTML = getMenuItems()
+        document.getElementById('order-display').innerHTML = yourOrder()
+        
+    }
+
+render()
